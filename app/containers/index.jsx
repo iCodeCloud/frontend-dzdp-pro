583f356a4e01bcb821a38df49a09d1c48369c482
icodecloud <icodecloud@163.com>
2017-09-05 15:58:42 +0800

    模拟loading，需要熟悉下react的声明周期。

--- app/containers/index.jsx
+++ app/containers/index.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 
 class App extends React.Component {
-
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            initDone: false // loading标识
+        };
     }
 
     render() {
         return (
             <div>
-                <p>header</p>
-                {this.props.children}
-                <p>footer</p>
+                {
+                    this.state.initDone
+                        ? this.props.children
+                        : <div>加载中...</div>
+                }
             </div>
         )
+    }
+
+    componentDidMount() {
+        // 1秒钟加载消失
+        setTimeout(() => {
+            this.setState({
+                initDone: true
+            })
+        }, 1000);
     }
 }
 
